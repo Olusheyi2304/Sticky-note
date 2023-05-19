42dbbbfa466875f50cb68c2c2e94650d262d4581
Patrick-Oteng <otengpatrickkwame@gmail.com>
2023-05-19 13:14:35 +0000

    finish adding the modal style to the note box

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 "use strict";
 const headerBtn = document.querySelector(".main-btn");
 const noteBoxBtn = document.querySelector(".add-note-btn");
+const noteBox = document.querySelector(".add-note-box");
 const closeBtn = document.querySelector(".close-svg");
+
+headerBtn.addEventListener("click", () => {
+  noteBox.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  noteBox.classList.add("hidden");
+});
+
+const keyFunc = (e) => {
+  if (e.key === "Escape" && !noteBox.classList.contains("hidden"))
+    noteBox.classList.add("hidden");
+};
+
+document.querySelector("body").addEventListener("keydown", keyFunc);
