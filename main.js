b3f87a399601ec43528b9c40cd7f7a3ec378df19
Patrick-Oteng <otengpatrickkwame@gmail.com>
2023-05-19 12:50:30 +0000

    add note box and hide with js

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+const headerBtn = document.querySelector(".main-btn");
+const noteBoxBtn = document.querySelector(".add-note-btn");
+const closeBtn = document.querySelector(".close-svg");
